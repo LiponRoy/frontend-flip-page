35061f288d65862f287a86a8b962ff6ead27a8a7
LiponRoy <liponbdbd@gmail.com>
2023-07-25 16:54:53 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className=' h-screen w-full flex justify-center items-center'>
+			<span>Flip Page</span>
+		</div>
+	);
+}
+
+export default App;
